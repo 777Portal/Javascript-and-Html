89a62abbe41888ae062693a0f7822f27d7a6936c
Transportal <127454667+777Portal@users.noreply.github.com>
2023-03-09 19:25:55 -0500

    trying to add favicon + adding background style to the page

--- bookmarklet page.js
+++ bookmarklet page.js
@@ -3,18 +3,21 @@ var value = 0;
 var increment = 1;
 var ceiling = 0;
 
-setInterval(ChangeTitle, 500);
-
+/*New tab*/
 var tab = window.open('about:blank', '_blank');
-tab.write
+tab.document.body.style.backgroundColor = "black"
 newTitle = ">>>BMH<<<"
 
+let favicon = document.createElement('link');
+favicon.innerHTML = 'rel="icon" type="image/x-icon" href="https://cdn-icons-png.flaticon.com/512/5968/5968523.png?"'
+tab.document.head.appendChild(favicon)
 
 /*hahah funny cosmetic title change that slows down the browser and the rest of the javascript go burrrr*/
+setInterval(ChangeTitle, 500);
 function ChangeTitle(){
   PerformCalc(9);
-   newTitleSub = newTitle.substring(0,value);
-   if (tab.document.title != newTitleSub) {
+  newTitleSub = newTitle.substring(0,value);
+  if (tab.document.title != newTitleSub) {
        tab.document.title = newTitleSub;
    }
 };
@@ -23,8 +26,6 @@ function PerformCalc(ceiling) {
    console.log(value)
  if (up === true && value <= ceiling) {
    value += increment
-
-
    if (value == ceiling) {
      up = false;
    }
